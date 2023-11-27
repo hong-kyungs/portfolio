d187d9c8ee43ac1d42f4c90c1b34acca4c54d815
hong-kyungs <hkyungs11@naver.com>
2023-11-27 17:03:28 +0900

    key값 변경

--- js/location.js
+++ js/location.js
@@ -1,70 +1,70 @@
-var mapContainer = document.getElementById("map");
+var mapContainer = document.getElementById('map');
 
-const branch_btns = document.querySelectorAll(".branch li");
+const branch_btns = document.querySelectorAll('.branch li');
 
 let drag = true;
 let zoom = true;
 
 var mapOption = {
-    center: new kakao.maps.LatLng(37.5101935, 127.0585824),
-    level: 3
+	center: new kakao.maps.LatLng(37.5101935, 127.0585824),
+	level: 3,
 };
-
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-var markerOptions =[
-    {
-        title : "headquater",
-        latlng : new kakao.maps.LatLng(37.5101935, 127.0585824),
-        button : branch_btns[0]
-    },
-    {
-        title : "branch1",
-        latlng : new kakao.maps.LatLng(37.624699, 127.3552654),
-        button : branch_btns[1]
-    },
-    {
-        title : "branch2",
-        latlng : new kakao.maps.LatLng(37.6960049, 128.8918872),
-        button : branch_btns[2]
-    }
+var markerOptions = [
+	{
+		title: 'headquater',
+		latlng: new kakao.maps.LatLng(37.5101935, 127.0585824),
+		button: branch_btns[0],
+	},
+	{
+		title: 'branch1',
+		latlng: new kakao.maps.LatLng(37.624699, 127.3552654),
+		button: branch_btns[1],
+	},
+	{
+		title: 'branch2',
+		latlng: new kakao.maps.LatLng(37.6960049, 128.8918872),
+		button: branch_btns[2],
+	},
 ];
 
-var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
-for(let i = 0; i < markerOptions.length; i++){
-    var imageSize = new kakao.maps.Size(24, 35);    
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+var imageSrc =
+	'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+for (let i = 0; i < markerOptions.length; i++) {
+	var imageSize = new kakao.maps.Size(24, 35);
+	var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-    new kakao.maps.Marker({
-        map : map,
-        position: markerOptions[i].latlng,
-        title : markerOptions[i].title,
-        image : markerImage
-    });
+	new kakao.maps.Marker({
+		map: map,
+		position: markerOptions[i].latlng,
+		title: markerOptions[i].title,
+		image: markerImage,
+	});
 
-    markerOptions[i].button.onclick = (e)=>{
-        e.preventDefault();
+	markerOptions[i].button.onclick = (e) => {
+		e.preventDefault();
 
-        for(let k=0; k<markerOptions.length; k++){
-        markerOptions[k].button.classList.remove("on");
-        }
-        markerOptions[i].button.classList.add("on");
+		for (let k = 0; k < markerOptions.length; k++) {
+			markerOptions[k].button.classList.remove('on');
+		}
+		markerOptions[i].button.classList.add('on');
 
-        moveTo(markerOptions[i].latlng);
-    }
+		moveTo(markerOptions[i].latlng);
+	};
 }
 
 setDraggable(drag);
-function setDraggable(draggable){
-    map.setDraggable(draggable);
+function setDraggable(draggable) {
+	map.setDraggable(draggable);
 }
 setZoomable(zoom);
-function setZoomable(zoomable){
-    map.setDraggable(zoomable);
+function setZoomable(zoomable) {
+	map.setDraggable(zoomable);
 }
 
-function moveTo(target){
-    var moveLatLon = target;
-    map.setCenter(moveLatLon);
+function moveTo(target) {
+	var moveLatLon = target;
+	map.setCenter(moveLatLon);
 }
--- js/youtube.js
+++ js/youtube.js
@@ -1,13 +1,5 @@
-/*
-API : application prgramming interface
-
-key : AIzaSyCVnqQLlCzUMqd8pqBHc34g-onr96Z0TaM
-
-playlist : PL0Rto-Av72qHWnhIdz3cs9CAEL0Z1R2Ln
-youtube에서 list뒤에 키 복사.
-*/
 const vidList = document.querySelector('.vidList');
-const key = 'AIzaSyCVnqQLlCzUMqd8pqBHc34g-onr96Z0TaM';
+const key = config.youtubeKey;
 const playlistId = 'PL0Rto-Av72qHWnhIdz3cs9CAEL0Z1R2Ln';
 const num = 6;
 const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
@@ -57,7 +49,6 @@ vidList.addEventListener('click', (e) => {
 	e.preventDefault();
 
 	const vidId = e.target.closest('a').getAttribute('href');
-
 	let pop = document.createElement('figure');
 	pop.classList.add('pop');
 	pop.innerHTML = `
@@ -69,6 +60,7 @@ vidList.addEventListener('click', (e) => {
 });
 
 vidList.addEventListener('click', (e) => {
+	// e.preventDefault();
 	const pop = vidList.querySelector('.pop');
 	if (pop) {
 		const close = pop.querySelector('span i');
