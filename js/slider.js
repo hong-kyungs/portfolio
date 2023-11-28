b57ff28b5e2df9d3be90b6b40dc06493527569f5
hong-kyungs <hkyungs11@naver.com>
2023-11-28 23:13:21 +0900

    이미지 크기 수정 & 모바일 버튼 수정

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,24 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "fadeIn",
-    direction:"horizontal",
-    spaceBetween:0,
-    slidePerView:1,
-    grabCursor:true,
-    loop:true,
-    autoplay:{ 
-      delay:3000, 
-      disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    mousewheel:true,
-    keyboard :{
-      enabled:true,
-    },
-  });
+var swiper = new Swiper('.mySwiper', {
+	effect: 'fadeIn',
+	direction: 'horizontal',
+	spaceBetween: 0,
+	slidePerView: 1,
+	grabCursor: true,
+	loop: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	mousewheel: true,
+	keyboard: {
+		enabled: true,
+	},
+});
